788efe9f085b8c8f843d433ac5d240888e942dc0
FkkLagom <larsson.benjamin@live.se>
2019-04-18 20:36:49 +0200

    Add project files.

---
+++ SnakeSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
